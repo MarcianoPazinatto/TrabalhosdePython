e472dd9133791abb6c7ed2df749c9b28312ea961
marciano <marciano.et@hotmail.com>
2019-11-14 11:50:45 -0300

    aula7

--- Aula7/aula7_3.py
+++ Aula7/aula7_3.py
@@ -5,12 +5,20 @@
 #----Imprima os jogadores e seus dados
 
 jogadores=[]#----cria uma lista, para poder colocar os valores do dicionário de todos os jogadores
-jogador={'nome':'','pos':'','num':'','per':'' }#----cria o dicionário, com os valores vázios
+
 for i in range(1,3):
+     jogador={'nome':'','pos':'','num':'','per':'' }#----cria o dicionário, com os valores vázios
+     
     jogador['nome']=(input('Nome: '))
     jogador['pos']=(input('Posição: '))
-    jogador['num']=(int(input('NUmero: ')))
+    jogador['num']=(int(input('Número: ')))
     jogador['per']=(input('Perna boa: '))
+   
     jogadores.append(jogador)#---adiciona o dicionário dentro da lista
+    
 
 print(jogadores)
+print('\n\n')
+
+for inf in jogadores:#dentro do jogadores, imprime os dados dos jogadores, ,ediante ao foreach
+    print(f"Nome do jogador: {jogador['nome']}-Posição: {jogador['pos']}-Número: {jogador['num']}-Perna: {jogador['per']}")
