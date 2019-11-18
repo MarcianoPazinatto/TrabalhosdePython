f6993fa50a37700f294e8144737fd70d64bea700
marciano <marciano.et@hotmail.com>
2019-11-18 11:32:04 -0300

    aula8

--- Aula8/aula8IF.py
+++ Aula8/aula8IF.py
@@ -1,14 +1,14 @@
 lista_numeros=[1,2,3,4]
 numero=2
 
-
+#conferindo se o número esta na lista
 if numero in lista_numeros:
     print('Existe')
 else:
     print('não existe') 
 
 lista_vazia=[]
-
+#o len conta quantos valores há na lista 
 if len(lista_vazia)==0:
     print('Vazia')
 else:
@@ -17,4 +17,3 @@ else:
 if len(lista_vazia)==0:
     print('vazia')
 
-    
