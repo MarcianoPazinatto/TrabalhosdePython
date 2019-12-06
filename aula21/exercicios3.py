f4b7c74ae883b9bf5f445de2606c94825ee48c81
Marciano <marciano.et@hotmail.com>
2019-12-06 11:51:48 -0300

    exer

---
+++ aula21/exercicios3.py
@@ -0,0 +1,27 @@
+# Aula 21 - 06-12-2019
+# Como Tratar e Trabalhar Erros!!!
+
+# 1 - Crie um metodo que leia 5 valores inteiros e retorne uma lista.
+# Esta função deve ter tratamento de excessão evitando erro ValueError.
+
+# 2 - Com a lista retornada, faça a multiplicação por 5 e salve o resultado
+# em outra lista.
+
+# Imprima a lista resultante
+
+def numeros():
+    lista=[]
+    for b in range(5):
+        b=int(input('Digite um numero inteiro: '))
+        lista.append(b)
+    return lista    
+
+a=numeros()
+print(a)
+
+listaum=[]
+for num in a:
+    mm=num*5
+    print(mm)
+    listaum.append(mm)
+    print(listaum)
