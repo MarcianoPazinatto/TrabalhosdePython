f655598a25afae6fb9e31512f6d44e3141bda87b
Marciano <marciano.et@hotmail.com>
2020-02-27 08:14:39 -0300

    Aula 61

---
+++ Aula61/cyclic.py
@@ -0,0 +1,19 @@
+#  A = [3, 8, 9, 7, 6]
+#  K = 3
+
+A = [3, 8, 9, 7, 6]
+
+for i in range(3):
+    A.insert(0, A[-1])
+    A.pop()
+print(A)
+
+# B = [1, 2, 3, 4]
+# K = 4
+
+B = [1, 2, 3, 4]
+
+for i in range(4):
+    B.insert(0,B[-1])
+    B.pop()
+print(B)
